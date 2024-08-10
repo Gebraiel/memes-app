045bfe2a392b42b7b08d9411290e2a894a528c08
Gebraiel <67439330+Gebraiel@users.noreply.github.com>
2024-08-10 18:18:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,29 +10,18 @@ input.addEventListener('keypress',(event)=>{
     event.key == "Enter" && setMeme();
 })
 function setMeme(){
-    if(input.value !=""){
-        if(!isNaN(input.value)&&input.value !=" "){
-            if(input.value >= 0 && input.value <= 99){
-                fetch('https://api.imgflip.com/get_memes').then((res)=>
-                    res.json()
-                ).then((data)=>{
-                    let meme = data.data.memes[input.value];
-                    let {name,url} = meme;
-                    console.log(meme);
-                    img.setAttribute('src',url);
-                    h1.innerHTML = name;
-                    input.value = "";
-                    error.innerHTML = "";
-                })
-            }else{
-                setError("Invalid value: please enter a valid number between 0 and 99");
-            }
-        }else{
-            setError("Invalid input: please enter a number");
-        }
-    }else{
-        setError("Empty Input:please enter a number between 0 and 99 ");
-    }
+    input.value == "" ? setError('Empty Input:please enter a number between 0 and 99'): isNaN(input.value) || input.value ==" " ? setError('Invalid input: please enter a number') : input.value < 0 || input.value > 99 ? setError("Invalid value: please enter a valid number between 0 and 99") :  fetch('https://api.imgflip.com/get_memes').then((res)=>
+        res.json()
+    ).then((data)=>{
+        let meme = data.data.memes[input.value];
+        let {name,url} = meme;
+        console.log(meme);
+        img.setAttribute('src',url);
+        h1.innerHTML = name;
+        input.value = "";
+        error.innerHTML = "";
+    }) 
+
 }
 function setError(message){
     img.setAttribute('src',"");
